4af4b8ead65db87de742901d93b20b88b45fcc51
Robert Carter <60618745+racarter1215@users.noreply.github.com>
2020-07-13 20:27:40 -0700

    Update Snake.cs

--- Lab06OOP/Lab06OOP/classes/Snake.cs
+++ Lab06OOP/Lab06OOP/classes/Snake.cs
@@ -1,19 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06_OOP
-{
+{/// <summary>
+/// below is the creation of a class that inherits from the base classes Reptile and Animal
+/// </summary>
     public abstract class Snake : Reptile
     {
         public bool Venemous { get; set; }
         public int Length { get; set; }
 
+        /// <summary>
+        /// below is a method that returns a integer when run
+        /// </summary>
+        /// <returns>integer</returns>
         public int Slither()
         {
             return 5;
         }
 
+        /// <summary>
+        /// below is a method that returns a string when run
+        /// </summary>
+        /// <returns>string</returns>
         public string Bite()
         {
             string BiteString = "Ow";
