af49015192d431c0282f928749dbe9e33293bb31
Robert Carter <60618745+racarter1215@users.noreply.github.com>
2020-07-13 20:24:13 -0700

    Update CongoLion.cs

--- Lab06OOP/Lab06OOP/classes/CongoLion.cs
+++ Lab06OOP/Lab06OOP/classes/CongoLion.cs
@@ -1,20 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06_OOP.classes
-{
+{/// <summary>
+/// below is the creation of the method CongoLion, which also inherits information from the base classes Mammal and Animal
+/// </summary>
     public class CongoLion : Mammal
     {
         public int PrideSize { get; set; }
 
         public bool HasMane { get; set; }
-
+        /// <summary>
+        /// below is a method that returns a string when run
+        /// </summary>
+        /// <returns>string</returns>
         public string Roar()
         {
             string RoarString = "Roar";
             return RoarString;
         }
+        /// <summary>
+        /// below is a method that returns a string when run
+        /// </summary>
+        /// <returns>string</returns>
 
         public string Hunt()
         {
