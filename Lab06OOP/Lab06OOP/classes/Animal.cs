6368e53af0e9c60948223666abbfd3b9100c5d78
Robert Carter <60618745+racarter1215@users.noreply.github.com>
2020-07-13 20:23:34 -0700

    Update Animal.cs

--- Lab06OOP/Lab06OOP/classes/Animal.cs
+++ Lab06OOP/Lab06OOP/classes/Animal.cs
@@ -1,21 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab06_OOP
-{
+{/// <summary>
+/// below is the creation of the abstract class Animal
+/// </summary>
     public abstract class Animal
     {
         public int Age { get; set; }
         public string Name { get; set; }
         public double Size { get; set; }
 
+        /// <summary>
+        /// below is a method that returns a string when GetFed is run
+        /// </summary>
+        /// <returns>string</returns>
         public string GetFed()
         {
             string FeedString = "Time for Lunch";
             return FeedString;
         }
 
+        /// <summary>
+        /// below is a method that returns a string when GetSleep is run
+        /// </summary>
+        /// <returns>string</returns>
         public string GetSleep()
         {
             string SleepString = "Time for Sleep";
