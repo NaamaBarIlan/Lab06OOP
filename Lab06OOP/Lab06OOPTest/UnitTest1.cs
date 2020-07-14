91095b720646383dd3d0f8dc0fe7a31d551e1e42
Robert Carter <racarter1215@gmail.com>
2020-07-13 18:39:38 -0700

    More tests complete

--- Lab06OOP/Lab06OOPTest/UnitTest1.cs
+++ Lab06OOP/Lab06OOPTest/UnitTest1.cs
@@ -17,5 +17,77 @@ namespace Lab06OOPTest
             //Assert
             Assert.Equal(message, result);
         }
+
+        [Fact]
+        public void OkapiSleepTest()
+        {
+            //Arrange
+            Okapi okapi = new Okapi();
+            string message = "Time for Sleep";
+            //Act
+            string result = okapi.GetSleep();
+            //Assert
+            Assert.Equal(message, result);
+        }
+
+        [Fact]
+        public void CongoLionAgeTest()
+        {
+            //Arrange
+            CongoLion lion = new CongoLion();
+            string message = "Time for Sleep";
+            //Act
+            string result = lion.GetSleep();
+            //Assert
+            Assert.Equal(message, result);
+        }
+
+        [Fact]
+        public void CongoLionMilkTest()
+        {
+            //Arrange
+            CongoLion lion = new CongoLion();
+            string message = "Produce Milk";
+            //Act
+            string result = lion.ProduceMilk();
+            //Assert
+            Assert.Equal(message, result);
+        }
+
+        [Fact]
+        public void MarinaRamFeedTest()
+        {
+            //Arrange
+            MarinaRam ram = new MarinaRam();
+            string message = "Time for Lunch";
+            //Act
+            string result = ram.GetFed();
+            //Assert
+            Assert.Equal(message, result);
+        }
+
+        [Fact]
+        public void MonitorLizardFeedTest()
+        {
+            //Arrange
+            MonitorLizard lizard = new MonitorLizard();
+            string message = "Time for Lunch";
+            //Act
+            string result = lizard.GetFed();
+            //Assert
+            Assert.Equal(message, result);
+        }
+
+        [Fact]
+        public void RattlesnakeSleepTest()
+        {
+            //Arrange
+            Rattlesnake snake = new Rattlesnake();
+            string message = "Time for Sleep";
+            //Act
+            string result = snake.GetSleep();
+            //Assert
+            Assert.Equal(message, result);
+        }
     }
 }
