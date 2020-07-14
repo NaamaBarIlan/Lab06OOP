0a609802f610da3e8190ea34d9b5bbb3a109ca65
Robert Carter <racarter1215@gmail.com>
2020-07-13 18:14:22 -0700

    please let this pull work

---
+++ Lab06OOP/Lab06OOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lab06_OOP.classes;
+using System;
+
+namespace Lab06_OOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LionTest();
+            RamTest();
+            OkapiTest();
+            LizardTest();
+            RattlesnakeTest();
+
+        }
+        /// <summary>
+        /// The following method proves that lion inherits informatin from base class Animal and Mammal
+        /// </summary>
+        static void LionTest()
+        {
+            CongoLion lion = new CongoLion();
+            int lionAge = lion.Age;
+            Console.WriteLine(lionAge);
+            
+        }
+        static void RamTest()
+        {
+            MarinaRam ram = new MarinaRam();
+            int ramAge = ram.Age;
+            Console.WriteLine(ramAge);
+        }
+        static void OkapiTest()
+        {
+            Okapi okapi = new Okapi();
+            int okapiAge = okapi.Age;
+            Console.WriteLine(okapiAge);
+        }
+        static void LizardTest()
+        {
+            MonitorLizard lizard = new MonitorLizard();
+            int lizardAge = lizard.Age;
+            Console.WriteLine(lizardAge);
+        }
+        static void RattlesnakeTest()
+        {
+            Rattlesnake snake = new Rattlesnake();
+            int snakeAge = snake.Age;
+            Console.WriteLine(snakeAge);
+        }
+    }
+}
---
+++ Lab06OOP/Lab06OOP/classes/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06_OOP
+{
+    abstract class Animal
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public double Size { get; set; }
+
+    }
+}
