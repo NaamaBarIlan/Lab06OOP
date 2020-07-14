112dfff259aeff3c4f1db75c41b557deb19b85ca
Robert Carter <60618745+racarter1215@users.noreply.github.com>
2020-07-13 20:22:51 -0700

    Update Program.cs

--- Lab06OOP/Lab06OOP/Program.cs
+++ Lab06OOP/Lab06OOP/Program.cs
@@ -1,10 +1,13 @@
-﻿using Lab06_OOP.classes;
+using Lab06_OOP.classes;
 using System;
 
 namespace Lab06_OOP
 {
     public class Program
-    {
+    {/// <summary>
+    /// The below method calls 5 subsequent methods (all from concrete derived objects)
+    /// </summary>
+    /// <param name="args"></param>
         static void Main(string[] args)
         {
             LionTest();
@@ -15,7 +18,7 @@ namespace Lab06_OOP
 
         }
         /// <summary>
-        /// The following method proves that lion inherits informatin from base class Animal and Mammal
+        /// The following method proves that CongoLion inherits info from base class Animal
         /// </summary>
         static void LionTest()
         {
@@ -24,24 +27,36 @@ namespace Lab06_OOP
             Console.WriteLine(lionAge);
             
         }
+        /// <summary>
+        /// the following method proves that MarinaRam inherits info from the base class Animal
+        /// </summary>
         static void RamTest()
         {
             MarinaRam ram = new MarinaRam();
             int ramAge = ram.Age;
             Console.WriteLine(ramAge);
         }
+        /// <summary>
+        /// the following method proves that Okapi inherits info from the base class Animal
+        /// </summary>
         static void OkapiTest()
         {
             Okapi okapi = new Okapi();
             int okapiAge = okapi.Age;
             Console.WriteLine(okapiAge);
         }
+        /// <summary>
+        /// the following method proves that MonitorLizard inherits info from the base class Animal
+        /// </summary>
         static void LizardTest()
         {
             MonitorLizard lizard = new MonitorLizard();
             int lizardAge = lizard.Age;
             Console.WriteLine(lizardAge);
         }
+        /// <summary>
+        /// the following method proves that Rattlesnake inherits info from the base class Animal
+        /// </summary>
         static void RattlesnakeTest()
         {
             Rattlesnake snake = new Rattlesnake();
